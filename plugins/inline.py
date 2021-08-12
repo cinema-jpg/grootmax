d20e1e75fd4f07c5e5b0e6b04d066cfb8db68e6a
cinema-jpg <85360750+cinema-jpg@users.noreply.github.com>
2021-08-12 09:12:17 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -72,7 +72,7 @@ async def answer(bot, query):
 def get_reply_markup(username):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
     buttons = [[
-        InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
+        InlineKeyboardButton('Search again', @cinema_zip''),
         InlineKeyboardButton('Share bot', url=url),
     ]]
     return InlineKeyboardMarkup(buttons)
